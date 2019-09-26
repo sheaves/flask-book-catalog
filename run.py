e6b7b13015d30fd249ba3c4a44425b5f7b7d88fc
wongliangze <liangze.wong@gmail.com>
2019-09-26 10:48:21 +0800

    Fixing deployment errors
    - Update psycopg2 version
    - Add error handling

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from app import create_app, db
 from app.auth.models import User
+from sqlalchemy import exc
 
 flask_app = create_app('prod')
 with flask_app.app_context():  # tells flask to use current application context
     db.create_all() # create database tables
-    if not User.query.filter_by(user_name = 'harry').first():
-        User.create_user(user = 'harry',
-                         email = 'harry@potter.com',
-                         password = 'secret')
-        flask_app.run()
+    try:
+        if not User.query.filter_by(user_name = 'harry').first():
+            User.create_user(user = 'harry',
+                             email = 'harry@potter.com',
+                             password = 'secret')
+    except exc.IntegrityError:
+            flask_app.run()
